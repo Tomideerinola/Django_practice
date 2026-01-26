0a9024d429c5b14d58fc23b55105383fa01cbee9
Tomide <tomideerinola@gmail.com>
2026-01-26 04:53:28 -0800

    learnt the crud activities using a form

--- applications/urls.py
+++ applications/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('jobs/', views.job_list, name='job_list'),  # our first page
+    path('', views.home, name='home'),
+    path('jobs/', views.job_list, name='job_list'),
+    path('jobs/add/', views.add_job, name='add_job'),
+    path('job/<int:pk>/edit/', views.edit_job, name='edit_job'),
+    path('job/<int:pk>/delete/', views.delete_job, name='delete_job'),
+    
 ]
--- applications/views.py
+++ applications/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import JobApplication
+from .forms import JobApplicationForm
+from django.contrib import messages
 
 # Create your views here.
 def job_list(request):
     jobs = JobApplication.objects.all()  # get all jobs from the database
     return render(request, 'applications/job_list.html', {'jobs': jobs})
 
+
+def home(request):
+    return render(request, 'applications/home.html')
+
+
+def add_job(request):
+    if request.method == 'POST':
+        form = JobApplicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('job_list')
+    else:
+        form = JobApplicationForm()
+
+    return render(request, 'applications/add_job.html', {'form': form})
+
+
+
+# --- EDIT JOB ---
+def edit_job(request, pk):
+    job = get_object_or_404(JobApplication, pk=pk)
+    if request.method == 'POST':
+        form = JobApplicationForm(request.POST, instance=job)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Job updated successfully!")
+            return redirect('job_list')
+    else:
+        form = JobApplicationForm(instance=job)
+    return render(request, 'applications/edit_job.html', {'form': form, 'job': job})
+
+# --- DELETE JOB ---
+def delete_job(request, pk):
+    job = get_object_or_404(JobApplication, pk=pk)
+    if request.method == 'POST':
+        job.delete()
+        messages.success(request, "Job deleted successfully!")
+        return redirect('job_list')
+    return render(request, 'applications/delete_job.html', {'job': job})
